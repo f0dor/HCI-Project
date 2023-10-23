2b26e1febcb9f3a3a597fe1b0f9780ce7971de7d
f0dor <markopcstuff@gmail.com>
2023-10-23 20:31:16 +0200

    Updated class-project 2 and added semester-project

---
+++ semester-project/src/app/showcase/page.tsx
@@ -0,0 +1,9 @@
+export default function ShowcasePage() {
+    return (
+        <div>
+            <h1>Showcase</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus soluta earum recusandae neque non optio, voluptate eligendi consectetur praesentium esse provident quae eveniet ipsa officiis culpa nam veritatis minima dignissimos.</p>
+        </div>
+    );
+
+}
