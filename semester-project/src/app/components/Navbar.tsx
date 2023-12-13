674c68e8330f2ca46a9f483ee792ebb627dd7448
f0dor <markopcstuff@gmail.com>
2023-12-13 16:08:39 +0100

    Compounded commits

---
+++ semester-project/src/app/components/Navbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { useState } from "react";
+
+import Logo from "./Logo";
+import MainNav from "./MainNav";
+import Hamburger from "./Hamburger";
+import MobileNav from "./MobileNav";
+
+export type Page = {
+  href: string;
+  title: string;
+};
+
+// Get this info from some external source (e.g. CMS)
+const pages: Page[] = [
+  { href: "/", title: "Home" },
+  { href: "/about", title: "About" },
+  { href: "/projects", title: "Projects" },
+  { href: "/skills", title: "Skills" },
+  { href: "/blog", title: "Blog" },
+  { href: "/contact", title: "Contact" },
+];
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="flex items-center justify-between">
+      <Logo />
+      <MainNav pages={pages} />
+      <Hamburger open={open} clickHandler={setOpen} />
+      <MobileNav open={open} clickHandler={setOpen} pages={pages} />
+    </div>
+  );
+};
+
+export default Navbar;
--- semester-project/src/app/projects/page.tsx
+++ semester-project/src/app/projects/page.tsx
@@ -1,7 +1,7 @@
 export default function ProjectsPage() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-10">
-        <h1 className="text-3xl font-bold">Projects</h1>
+        <h1 className="text-3xl font-bold">Projects Page</h1>
       </main>
     );
   }
