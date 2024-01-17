8f3e6c9182dd7845e6a977911f7b83d651e35141
f0dor <markopcstuff@gmail.com>
2024-01-17 16:53:16 +0100

    more progress

--- semester-project/src/app/components/HeroSection.tsx
+++ semester-project/src/app/components/HeroSection.tsx
@@ -1,22 +1,25 @@
 import "../custom.css";
+import Image from 'next/image'
 
 const HeroSection = () => {
   return (
-    <div className="text-white py-20 flex">
-      <div className="container mx-auto text-left flex-1">
-        <h1 className="text-4xl font-roboto-mono mb-4">Welcome to Our Website</h1>
-        <p className="text-3xl whitespace-wrap font-roboto-mono">Diving into the world of code and circuits.
-          I'm <span className="text-logo-font-color font-roboto-mono tracking-widest font-bold text-3xl whitespace-nowrap">&lt;f0dor/&gt;</span>, a tech enthusiast with a vision to engineer the future. 
+    <div className="text-white py-20 flex flex-wrap">
+      {/* Text Part */}
+      <div className="container mx-auto text-left flex-1 py-40 px-60 sm:px-20 md:px-40 lg:px-40">
+        <p className="text-3xl text-center whitespace-wrap font-roboto-mono mx-auto sm:px-2 md:px-5 lg:px-10">Diving into the world of code and circuits.
+          I'm <span className="text-logo-font-color font-roboto-mono tracking-widest font-bold text-3xl whitespace-nowrap">&lt; f0dor /&gt;</span>, a tech enthusiast with a vision to engineer the future. 
           Join me on this technological adventure.</p>
-        {/* Add more content as needed */}
       </div>
-      <div className="flex-1">
-        {/* Add your image here */}
-        <img src="../images/hsimage.png" alt="Hero section image" className="w-full h-auto" />
+
+      {/* Image Part */}
+      <div className="flex-1 hidden md:block pl-40">
+        {/* 'hidden md:block' will hide the image on screens smaller than md (medium) size */}
+        <Image src="/images/hsimage.png" alt="Hero section image" width={500} height={500} />
       </div>
     </div>
   );
 };
 
 
-  export default HeroSection;
+
+export default HeroSection;
--- semester-project/src/app/components/MainNav.tsx
+++ semester-project/src/app/components/MainNav.tsx
@@ -9,16 +9,16 @@ import "../custom.css";
 const MainNav = ({ pages }: { pages: Page[] }) => {
   const pathname = usePathname();
   return (
-    <nav className="hidden lg:flex items-center justify-center px-4 py-6">
+    <nav className="hidden lg:flex items-center justify-center px-2 py-6">
       <ul className="flex gap-2">
         {pages.map(({ href, title }) => (
           <li key={href}>
             <Link href={href}>
               <span
                 className={cn(
-                  "uppercase whitespace-nowrap font-roboto-mono text-base px-5 py-3 rounded-sm text-nav-font-color bg-brand-purple-100 transition-transform hover:shadow-md hover:text-white",
+                  "uppercase whitespace-nowrap font-roboto-mono text-base px-2 py-3 rounded-sm text-nav-font-color bg-brand-purple-100 transition-transform hover:shadow-md hover:text-white",
                   {
-                    "bg-brand-purple-700 text-logo-font-color pointer-events-none":
+                    "bg-brand-purple-700 text-logo-font-color pointer-events-none text-xl":
                       pathname === href,
                   }
                 )}
