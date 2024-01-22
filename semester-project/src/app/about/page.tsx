ea036c4bc4c2c147380cdd9bb323c6e56376e738
f0dor <markopcstuff@gmail.com>
2024-01-22 14:25:51 +0100

    fixes

--- semester-project/src/app/about/page.tsx
+++ semester-project/src/app/about/page.tsx
@@ -2,17 +2,17 @@ export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-10">
       <div className="about-padding-main text-white text-center font-roboto-mono text-2xl max-sm:text-sm max-md:text-lg">
-        <p className=" width-about padding-p-about">Hello! My name is Marko. I’m from Croatia and I’m interested in becoming a computer engineer.
+        <p className="width-about padding-p-about">Hello! My name is Marko. I’m from Croatia and I’m interested in becoming a computer engineer.
           I like coding, math, physics, electronics, philosophy, girls, you name it.
         </p>
-        <p className=" width-about padding-p-about">I’m currently studying computer science at FESB in Split.
+        <p className="width-about padding-p-about">I’m currently studying computer science at FESB in Split.
           I’m 23 years old and I spend my free time volunteering, reading, learning random stuff, etc. </p>
-        <p className=" width-about padding-p-about">I enjoy coding in C and C++. Embedded computing is something I’m interested in. 
+        <p className="width-about padding-p-about">I enjoy coding in C and C++. Embedded computing is something I’m interested in. 
         Working 
         with 
         micro controllers might be stressful sometimes but when it works, it’s just so satisfying being able to physically see something you programmed work as intended.
         </p>
-        <p className=" width-about padding-p-about">I’m not just interested in embedded software. I always wanted to try game development, or networking stuff, or be a systems designer.
+        <p className="width-about padding-p-about">I’m not just interested in embedded software. I always wanted to try game development, or networking stuff, or be a systems designer.
         </p>
       </div>
     </main>
