7cb276a52ce1cd5e160733a79b7f9f730494a10b
stipe-jurkovic <sjurko00@fesb.hr>
2024-01-22 15:45:48 +0100

    mesedž

--- semester-project/src/app/components/Project.tsx
+++ semester-project/src/app/components/Project.tsx
@@ -4,18 +4,19 @@ import Image from 'next/image'
 const ProjectCard = () => {
     return (
         <div className="text-white font-roboto-mono py-5 flex flex-row items-center custom-background margin-project">
-                <Image src="/images/projectphoto.jpg" alt="Hero section image" width={200} height={200} className="project-image" />
-                <div className="flex flex-col justify-center items-center margin-project-text" style={{ height: '100%', width: '50%'}}>
-                    <h1 className="py-1 text-center text-lg pb-5">DEVICE FOR ACCURATE MEASUREMENT OF SPEED AND TIME IN SPORTS ACTIVITIES</h1>
-                    <p className="py-1 text-sm">This project describes the development of a device for accurate measurement of speed 
+            <Image src="/images/projectphoto.jpg" alt="Hero section image" width={200} height={200} className="project-image" />
+            <div className="flex flex-col justify-center items-start margin-project-text" style={{ height: '100%', width: "fit-content" }}>
+                <h1 className="py-1 text-center text-lg text-logo-font-color">DEVICE FOR ACCURATE MEASUREMENT OF SPEED AND TIME IN SPORTS ACTIVITIES</h1>
+                <p className="py-1 text-sm">This project describes the development of a device for accurate measurement of speed
                     and time in sports activities using the ESP32-C3 micro controller and BLE technology.
-                        The device enables users to track their sports performance with a maximum error of +/- 50 milliseconds.</p>
-                    <Image src="/images/github.png" alt="Hero section image" width={50} height={50} className="py-1"/>
-                </div>
+                    The device enables users to track their sports performance with a maximum error of +/- 50 milliseconds.</p>
+                <a href="https://github.com/f0dor/Strukture-Podataka" className="py-1 flex flex-row">
+                    <Image src="/images/github.png" alt="Hero section image" width={25} height={25} />
+                    <span> &nbsp;github</span>
+                </a>
+            </div>
         </div>
     );
 };
-
-
 
 export default ProjectCard;
