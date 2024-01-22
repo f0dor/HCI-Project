df89cbc28320abd1f9080475d279ca9fcdc5444d
f0dor <markopcstuff@gmail.com>
2024-01-22 14:09:57 +0100

    fixes

--- semester-project/src/app/components/Project.tsx
+++ semester-project/src/app/components/Project.tsx
@@ -5,7 +5,7 @@ const ProjectCard = () => {
     return (
         <div className="text-white font-roboto-mono py-5 flex flex-row items-center custom-background margin-project">
                 <Image src="/images/projectphoto.jpg" alt="Hero section image" width={200} height={200} className="project-image" />
-                <div className="flex flex-col justify-center items-center margin-project-text" style={{ height: '100%', width: '50%'}}>
+                <div className="flex flex-col justify-center items-center margin-project-text" style={{ height: '100%', width: '60%'}}>
                     <h1 className="py-1 text-center text-lg pb-5">DEVICE FOR ACCURATE MEASUREMENT OF SPEED AND TIME IN SPORTS ACTIVITIES</h1>
                     <p className="py-1 text-sm">This project describes the development of a device for accurate measurement of speed 
                     and time in sports activities using the ESP32-C3 micro controller and BLE technology.
