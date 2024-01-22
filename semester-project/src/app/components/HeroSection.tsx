635d1b72d38b8ed9b34b6a05979d6ec431a9781f
stipe-jurkovic <sjurko00@fesb.hr>
2024-01-22 15:55:41 +0100

    popravljen '

--- semester-project/src/app/components/HeroSection.tsx
+++ semester-project/src/app/components/HeroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
     <div className="text-white py-20 hero-flex">
       <div className="container mx-auto text-left flex-1 py-40 px-40 max-sm:px-5 max-md:px-10 max-lg:px-20">
         <p className="text-3xl text-left whitespace-wrap font-roboto-mono mx-auto sm:px-2 md:px-5 lg:px-10 max-sm:text-md max-md:text-xl">Diving into the world of code and circuits.
-          I'm <span className="text-logo-font-color font-roboto-mono tracking-widest font-bold text-3xl whitespace-nowrap">&lt;f0dor/&gt;</span>, a tech enthusiast with a vision to engineer the future. 
+          I&apos;m <span className="text-logo-font-color font-roboto-mono tracking-widest font-bold text-3xl whitespace-nowrap">&lt;f0dor/&gt;</span>, a tech enthusiast with a vision to engineer the future. 
           Join me on this technological adventure.</p>
       </div>
 
